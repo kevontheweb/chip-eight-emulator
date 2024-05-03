8512918de09944908db729f3fc644c472232a55f
kevin <78508907+kevin-nel@users.noreply.github.com>
2024-05-03 19:41:24 +0200

    handle keypresses

--- src/main.rs
+++ src/main.rs
@@ -49,43 +49,36 @@ fn main() {
     'gameloop: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'gameloop;
+                }
+                Event::KeyDown {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(button) = keypress_to_button_code(key) {
+                        chip_eight.keypress(button, true);
+                    }
+                }
+                Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(button) = keypress_to_button_code(key) {
+                        chip_eight.keypress(button, false);
+                    }
                 }
                 _ => {}
             }
         }
         for _ in 0..TICKS_PER_FRAME {
+            // 10 instructions per frame
             chip_eight.tick(); //execute next instruction (move program counter)
         }
         chip_eight.tick_timers();
         draw_screen(&chip_eight, &mut canvas);
-    }
-}
-
-struct Keys {
-    up: bool,
-    down: bool,
-    left: bool,
-    right: bool,
-    a: bool,
-    b: bool,
-    c: bool,
-}
-impl Keys {
-    fn new() -> Self {
-        Self {
-            up: false,
-            down: false,
-            left: false,
-            right: false,
-            a: false,
-            b: false,
-            c: false,
-        }
-    }
-    fn read() -> Self {
-        todo!();
     }
 }
 
@@ -109,4 +102,32 @@ fn draw_screen(emulator: &Emulator, canvas: &mut Canvas<Window>) {
         }
     }
     canvas.present();
+}
+
+fn keypress_to_button_code(key: Keycode) -> Option<usize> {
+    // original chip 8 expects a grid of 4x4 buttons
+    // 1 2 3 C
+    // 4 5 6 D
+    // 7 8 9 E
+    // A 0 B F
+    // this function maps the sdl2 keycode to the chip8 button code
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
 }
